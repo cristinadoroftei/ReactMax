7c4e6297b22896786670b468fded47f9e0166934
Cristina <dorofteicristina19@gmail.com>
2020-07-17 13:18:02 +0200

    idk

--- .history/src/pages/Feed/Feed_20200717103626.js
+++ .history/src/pages/Feed/Feed_20200717103626.js
@@ -38,6 +38,34 @@ class Feed extends Component {
     this.loadPosts();
   }
 
+  addPost = post => {
+    this.setState(prevState => {
+      const updatedPosts = [...prevState.posts];
+      if (prevState.postPage === 1) {
+        if (prevState.posts.length >= 2) {
+          updatedPosts.pop();
+        }
+        updatedPosts.unshift(post);
+      }
+      return {
+        posts: updatedPosts,
+        totalPosts: prevState.totalPosts + 1
+      };
+    });
+  }
+
+  updatePost = post => {
+    this.setState(prevState => {
+      const updatedPosts = [...prevState.posts];
+      const updatedPostIndex = updatedPosts.findIndex(p => p._id === post._id);
+      if(updatedPostIndex > -1) {
+        updatedPosts[updatedPostIndex] = post
+      }
+      return {
+        posts: updatedPosts
+      }
+    })
+  }
 
   loadPosts = direction => {
     if (direction) {
